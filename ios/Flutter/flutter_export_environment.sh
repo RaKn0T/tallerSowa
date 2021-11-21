24bc89e36e4313e9380d617e13d5e5b088d0deea
dabgg <dieghoh123@hotmail.com>
2021-11-21 16:20:13 -0300

    cambios2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\danie\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\danie\Downloads\Flutter_Google_Maps_MyTrack-master"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\diegh\OneDrive\Escritorio\SowaAPP\Flutter_Google_Maps_MyTrack-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
